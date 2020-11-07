1bff910a2a77e8b89dbe990a7ac59c5c2d2ff8bd
Jeff Eckert <bromodo@gmail.com>
2020-11-07 14:26:10 -0500

    added items to transactions db item_id, ammount, note and timestamp

--- db/migrate/20201107191805_create_transactions.rb
+++ db/migrate/20201107191805_create_transactions.rb
@@ -1,7 +1,9 @@
 class CreateTransactions < ActiveRecord::Migration[6.0]
   def change
     create_table :transactions do |t|
-
+      t.integer :item_id
+      t.integer :amount
+      t.string :note
       t.timestamps
     end
   end
