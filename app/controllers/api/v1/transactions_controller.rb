678654148d9842ebec54fbda68e73bc857628b33
Jeff Eckert <bromodo@gmail.com>
2020-11-08 19:41:28 -0500

    added set_item method and transaction params to transaction controller also created a before_action for set_item

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::TransactionsController < ApplicationController
 
+    before_action :set_item
    
+
+    private
+
+    def set_item
+        @item = Item.find(params[:item_id])
+    end
+
+    def transaction_params
+        params.require(:transaction).permit(:item_id, :amount, :kind, :note, :date)
+    end
 
 end
