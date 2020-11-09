a43d8fa5f270a83d29ad923de5cdda91f7417ef2
Jeff Eckert <bromodo@gmail.com>
2020-11-08 19:57:10 -0500

    set up nested route for transactions

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :transactions
-      resources :items
+      resources :items do
+        resources :transactions
+      end
     end
   end
  
