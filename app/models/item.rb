fca405263e5ea2fc2ae383a6b360aa865eed6e75
Jeff Eckert <bromodo@gmail.com>
2020-11-08 20:01:51 -0500

    added update_balance_on_delete method to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,32 @@
 class Item < ApplicationRecord
-    has_many :transactions
-    validates :name, :balance, presence: true
+  has_many :transactions
+  validates :name, :balance, presence: true
 
-    def update_balance(transaction)
-        if transaction.kind == 'add'
-          self.balance = self.balance + transaction.amount
-          self.save
-        elsif transaction.kind == 'subtract'
-          if self.balance >= transaction.amount
-            self.balance = self.balance - transaction.amount
-            self.save
-          else
-            return 'Balance too low.'
-          end
-        end
+  def update_balance(transaction)
+    if transaction.kind == 'add'
+        elf.balance = self.balance + transaction.amount
+        self.save
+    elsif transaction.kind == 'subtract'
+     if self.balance >= transaction.amount
+        self.balance = self.balance - transaction.amount
+         self.save
+    else
+        return 'Balance too low.'
+      end
+   end
+ end
+
+  def update_balance_on_delete(transaction)
+    if transaction.kind == 'deposit'
+      if self.balance >= transaction.amount
+       self.balance = self.balance - transaction.amount
+       self.save
+      else
+        return 'Balance too low.'
+     end
+    elsif transaction.kind == 'withdraw'
+      self.balance = self.balance + transaction.amount
+      self.save
     end
+  end
 end
