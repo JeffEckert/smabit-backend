9ce6d0c98938ced24782d622785c0732299a5f88
Jeff Eckert <bromodo@gmail.com>
2020-11-08 19:08:59 -0500

    framed  out inital controller methods including index,create, show,destroy and item_params

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::ItemsController < ApplicationController
+
+    def index
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def item_params
+
+    end
 end
