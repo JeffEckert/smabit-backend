5b82e43b45e8fe14cb7a0c01f3a8fddcf05164d6
Jeff Eckert <bromodo@gmail.com>
2020-11-19 20:05:08 -0500

    added item_id to serializer

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,3 +1,9 @@
 class TransactionSerializer < ActiveModel::Serializer
-  attributes :id, :amount, :kind, :date, :note
+  attributes :id, :amount, :kind, :date, :note, :item_id
+
+  def date
+
+    self.object.date.strftime("%m/%d/%Y %I:%M%p")
+  end
+
 end
